ad6d65a513316b2349e8ec73d1dac1425c6038c9
CalOrange <calorange@CalOranges-MacBook-Pro.local>
2016-07-29 13:31:35 -0400

    create simple treasure map

--- pirate.py
+++ pirate.py
@@ -9,4 +9,6 @@ with open('treasure.txt') as fh:
   for line in fh:
     print line
     
-    
+with open('map.txt', 'w') as fh:
+  for i in range(10):
+    fh.write("Walk " + str(i) + " paces left.")
